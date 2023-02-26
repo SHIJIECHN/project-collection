c3a22026909765efe6ffd6b73468249c7bc09420
shijie <shijiechn@aliyun.com>
2023-02-26 22:44:07 +0800

    react

--- React/zhufeng/example/src/index.js
+++ React/zhufeng/example/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // React严格模式和use strict不一样
   <React.StrictMode>
     <div>123</div>
   </React.StrictMode>
