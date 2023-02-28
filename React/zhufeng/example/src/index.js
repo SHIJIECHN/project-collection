deb91e8016400ad1a3d9caad4a174652f5cdf7c0
shijie <shijiechn@aliyun.com>
2023-02-28 22:57:54 +0800

    react

--- React/zhufeng/example/src/index.js
+++ React/zhufeng/example/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@/index.less'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -8,3 +9,15 @@ root.render(
     <div>123</div>
   </React.StrictMode>
 );
+
+// fetch('/jian/subscriptions/recommended_collections')
+//   .then(response => response.json())
+//   .then(value => {
+//     console.log(value);
+//   })
+
+// fetch('/zhi/news/latest')
+//   .then(response => response.json())
+//   .then(value => {
+//     console.log(value);
+//   })
